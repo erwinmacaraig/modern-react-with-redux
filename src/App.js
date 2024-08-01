5724aaac1cf304323b09025cb1836a470eee0ecd
Erwin Macaraig <erwin.macaraig@gmail.com>
2024-08-01 23:01:44 +0800

    adding bulma css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// when we are importing folders of files out of the node modules directory
+// we do not have to use a relative path
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
@@ -8,10 +11,30 @@ function App(){
     return (
         <>
             <div>
-                <div>Personal Digital Assistants</div>
-                <ProfileCard title="Alexa" handle="@alexa99" />
-                <ProfileCard title="Cortana" handle="@cortana32" />
-                <ProfileCard title="Siri" handle="@siri01" />
+                <section className='hero is-primary'>
+                    <div className='hero-body'>
+                        <p className='title'>Personal Digital Assistants</p>
+                    </div>
+                </section>
+                
+                <div className='container'>
+                    <section className='section'>
+                        <div className='columns'>
+                            <div className='column is-4'>
+                                <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} description='Alexa was created by Amazon and helps you buy things' />
+                            </div>
+                            <div className='column is-4'>
+                                 <ProfileCard title="Cortna" handle="@cortana32" image={CortanaImage} description="Cortana was made by Microsoft" />  
+                            </div>
+                            <div className='column is-4'>
+                                <ProfileCard title="Siri" handle="@siri01" image={SiriImage} description="Siri was made by Applye and is about to be phased out" />
+                            </div>
+                            
+                        </div>
+                    </section>
+                </div>
+                
+                
             </div>
         </>
     );
