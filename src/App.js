29fe2ae63e1723fb89c7e37245362921ac53c459
Erwin Macaraig <erwin.macaraig@gmail.com>
2024-08-06 11:30:25 +0800

    finalize search for images

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 // when we are importing folders of files out of the node modules directory
 // we do not have to use a relative path
-import { useState } from 'react';
+
 import 'bulma/css/bulma.css';
 import './App.css'; 
 import PDA from './pages/PDA';
 import Animals from './pages/Animals';
+import Search from './pages/Search';
 //==========================
-import SearchBar from './components/SearchBar';
-import ImageList from './components/ImageList';
-import searchImages from './api';
+
 
 
 function App(){
-    const [images, setImages] = useState([]);
-    const handleSubmit = async (term) => {
-        const result = await searchImages(term);
-        setImages(result);
-    }
-    return (
-        <div>
-            <SearchBar onSubmit={handleSubmit} />
-            <ImageList images={images} />
-        </div>
-    );    
+       return <Search />
 }
 
 export default App;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,14 @@
+import './ImageList.css';
+import ImageShow from './ImageShow';
 
 function ImageList({images}){
+    const renderedImages = images.map((image) => {
+        return <ImageShow key={image.id} image={image} />
+    });
     return (
-        <div>ImageList: {images.length}</div>
+        <div className='image-list'>
+            {renderedImages}
+        </div>
     );
 }
 
