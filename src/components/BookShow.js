d8306cf6f0f519433cbe7f1bc0ce9a51a07826d9
Erwin Macaraig <erwin.macaraig@gmail.com>
2024-08-07 11:52:11 +0800

    adding images
    demo of string interpolation

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -22,6 +22,7 @@ function BookShow({onDelete, book, onEdit}){
     }
     return (
         <div className="book-show">
+            <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
         {content}
         <div className="actions">
             <button className='edit' onClick={handleEditClick}>Edit</button>
