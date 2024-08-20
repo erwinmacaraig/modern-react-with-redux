e68ed7cdfdad93b383700d826fa22e4464ab67d2
Erwin Macaraig <erwin.macaraig@gmail.com>
2024-08-20 22:44:43 +0800

    use the fetch api together with useEffect

--- src/App.js
+++ src/App.js
@@ -8,12 +8,27 @@ import Animals from './pages/Animals';
 import Search from './pages/Search';
 //==========================
 
-import {useState } from 'react';
+import {useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate'; 
 import BookList from './components/BookList';
 
 function App(){
        const [books, setBooks] = useState([]);
+
+       const fetchBooks = async () => {
+              const response = await fetch('http://localhost:3001/books', {
+                     method: 'GET',
+                     headers: {
+                            'Content-Type': 'application/json'
+                     }
+              });
+              const responseData = await response.json();
+              setBooks(responseData);
+       };
+
+       useEffect(() => {
+              fetchBooks();
+       }, [])
        const createBook = async (title) => {
               const response = await fetch('http://localhost:3001/books', {
                      method: 'POST',
@@ -31,17 +46,32 @@ function App(){
               setBooks(updatedBooks);
        }
 
-       const editBookById = (id, newTitle) => {
+       const editBookById = async (id, newTitle) => {
+              let response = await fetch('http://localhost:3001/books/' + id, {
+                     method: 'PUT',
+                     headers: {
+                            'Content-Type': 'application/json'
+                     },
+                     body: JSON.stringify({title: newTitle})
+              });
+              let data = await response.json();
+              console.log(data);
               const updatedBooks = books.map((book) => {
                      if (book.id === id) {
-                            return {...book, title: newTitle};
+                            return {...book, ...data};
                      }
                      return book;
               });
               setBooks(updatedBooks);
        };
 
-       const deleteBookById = (id) => {
+       const deleteBookById = async (id) => {
+              let response = await fetch(`http://localhost:3001/books/${id}`, {
+                     method: 'DELETE',
+                     headers: {
+                            'Content-Type': 'application/json'
+                     }
+              });
               const updatedBooks = books.filter((book) => {
                      return book.id !== id;
               });
